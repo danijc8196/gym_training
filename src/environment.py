7eb88dfa41cdb5f887c72b1657c124238e23c516
danijc8196 <danijc8196@gmail.com>
2018-10-17 22:29:45 +0200

    Work in progress. Bug to fix: offboard_mode flag is not being well recognized

--- src/environment.py
+++ src/environment.py
@@ -8,6 +8,10 @@ import airsim_connection as simulator
 import start_training as main
 import gym
 from gym.envs.registration import register
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import quaternion_from_euler, quaternion_multiply
+
+PI = 3.14159265359
 
 # Register the training environment in the gym as an available one
 reg = register(
@@ -20,16 +24,20 @@ class QuadCopterEnv(gym.Env):
 
 	def __init__(self):
 		# Initialize ros topics needed for taking observations / send commands
-		offb_ctrl_pub = rospy.Publisher('offboard/control', Bool, queue_size=1)
-		pose_pub = rospy.Publisher('/offb/pose', PoseStamped, queue_size=10)
-        pub_twist_pub = rospy.Publisher('/offb/twist', Twist, queue_size=10)
-        command_pub = rospy.Publisher('/offb/command', String, queue_size=10)
+		self.offb_ctrl_pub = rospy.Publisher('offb/control', Bool, queue_size=1)
+		self.desired_pose_pub = rospy.Publisher('offb/pose', PoseStamped, queue_size=1)
 
-		# Stablish connection with sim and connect the offboard mode
+
+		# Stablish connection with sim, connect the offboard mode and takeoff to initial position
 		self.sim = simulator.AirSimConnection()
-		time.sleep(5)
-		offb_ctrl_pub.publish(Bool(True))
-		
+		time.sleep(3)
+		self.set_initial_position()
+		self.offb_ctrl_pub.publish(Bool(True))
+		time.sleep(3)
+
+		time.sleep(10)
+		self.offb_ctrl_pub.publish(False)
+
 		self.action_space = agent.ACTION_SPACE
 		
 
@@ -44,6 +52,7 @@ class QuadCopterEnv(gym.Env):
 		"""
 		This functions returns the environment to the initial state
 		"""
+
 		# TODO:
 		# (land?) & disarm
 		# offboard OFF
@@ -73,3 +82,17 @@ class QuadCopterEnv(gym.Env):
 		reward = 0
 		done = False
 		return state, reward, done
+
+	def set_initial_position(self):
+		desired_pose = PoseStamped()
+		desired_pose.pose.position.x = 0
+		desired_pose.pose.position.y = 0
+		desired_pose.pose.position.z = 10
+		q_orig = quaternion_from_euler(0, 0, 0)
+		q_rot = quaternion_from_euler(0, 0, PI/4)
+		q_new = quaternion_multiply(q_rot, q_orig)
+		desired_pose.pose.orientation.x = q_new[0]
+		desired_pose.pose.orientation.y = q_new[1]
+		desired_pose.pose.orientation.z = q_new[2]
+		desired_pose.pose.orientation.w = q_new[3]
+		self.desired_pose_pub.publish(desired_pose)
--- src/offboard_node.py
+++ src/offboard_node.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
-import rospy
+import rospy, time
 from tf.transformations import quaternion_from_euler, quaternion_multiply
-from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped, Twist, Vector3
+from std_msgs.msg import String, Bool
+from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
 from mavros_msgs.srv import SetMode, CommandBool
 
 PI = 3.14159265359
 
+
+def offb_mode_cb(flag):
+    global offboard_mode
+    offboard_mode = flag
+    print("OFFB: received offb_mode_cb " + str(flag))
 
 def state_cb(state): # Connection state with autopilot
     global current_state
@@ -22,145 +27,99 @@ def desired_pose_cb(pose): # Desired pose to send
     global desired_pose
     desired_pose = pose
 
-def desired_twist_cb(twist): # Desired twist to send
-    global desired_twist
-    desired_twist = twist
-
-def desired_command_cb(command): # Desired command
-    global takeoff
-    global fordward
-    global rotation
-    print("Received command: "+command.data)
-    if command.data == "RESET_FLAGS":
-        takeoff = False
-        fordward = False
-        rotation = False
-    elif command.data == "TAKEOFF":
-        takeoff = True
-    elif command.data == "FORDWARD":
-        fordward = True
-    elif command.data == "ROTATION":
-        rotation = True
-    else:
-        pass
-
 
 if __name__ == '__main__':
     try:
+
+        rospy.init_node('offboard_node', anonymous=True)
 
         # Variables
         current_state = State()
         current_pose = PoseStamped()
         desired_pose = PoseStamped()
-        desired_twist = Twist()# Time rate
+        offboard_mode = False
+        
+        # Time rate
         rate = rospy.Rate(20)
-
-        # Config default pose values (quadrotor landed looking in front)
-        desired_pose.pose.position.x = 0
-        desired_pose.pose.position.y = 0
-        desired_pose.pose.position.z = 0
-        q_orig = quaternion_from_euler(0, 0, 0)
-        q_rot = quaternion_from_euler(0, 0, PI/2)
-        q_new = quaternion_multiply(q_rot, q_orig)
-        desired_pose.pose.orientation.x = q_new[0]
-        desired_pose.pose.orientation.y = q_new[1]
-        desired_pose.pose.orientation.z = q_new[2]
-        desired_pose.pose.orientation.w = q_new[3]
-
-        # Config default twist values (No traslation, No rotation)
-        defaultTwist = Twist()
-        defaultTwist.linear = Vector3(0,0,0)
-        defaultTwist.angular = Vector3(0,0,0)
-
-        # Initialize flags
-        takeoff = False
-        fordward = False
-        rotation = False
-    
-        rospy.init_node('offboard_node', anonymous=True)
         
         # Define publishers and subscribers
         pose_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
-        twist_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel_unstamped', Twist, queue_size=10)
         
+        offb_mode_sub = rospy.Subscriber('offb/control', Bool, offb_mode_cb)
         state_sub = rospy.Subscriber('mavros/state', State, state_cb)
         current_pose_sub = rospy.Subscriber('mavros/local_position/pose', PoseStamped, current_pose_cb)
         desired_pose_sub = rospy.Subscriber('offb/pose', PoseStamped, desired_pose_cb)
-        desired_twist_sub = rospy.Subscriber('offb/twist', Twist, desired_twist_cb)
-        desired_command_sub = rospy.Subscriber('offb/command', String, desired_command_cb)
         
-        # Connect to services
-        try:
-            rospy.wait_for_service('mavros/set_mode', 10)
-            rospy.wait_for_service('mavros/cmd/arming', 10)
-        except rospy.ROSException:
-            print("Failed to connect to services")
-        
-        # Start setting OFFBOARD mode on autopilot
-        while not current_state.connected:
-            rate.sleep()
 
-        for i in range(1,25):
-            pose_pub.publish(desired_pose)
-            rate.sleep()
+        while True:
+            print("while true")
+            if offboard_mode == True:
+                print("Estamos en el IF")
 
-        set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
-        response_setmode = set_mode_client.call(0, "OFFBOARD")
-        print("Response mode ------")
-        print(response_setmode)
-        print("------")
-    
-        arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
-        response_arming = arming_client.call(True)
-        print("Response arm ------")
-        print(response_arming)
-        print("------")
+                # Connect to services
+                try:
+                    rospy.wait_for_service('mavros/set_mode', 10)
+                    rospy.wait_for_service('mavros/cmd/arming', 10)
+                    print("Services connected")
+                except rospy.ROSException:
+                    print("Failed to connect to services")
+                
+                # Start setting OFFBOARD mode on autopilot
+                while not current_state.connected:
+                    rate.sleep()
 
-        print("-- OFFBOARD MODE TURNED ON --")
-
-        # Keeping OFFBOARD mode alive ---------------------------------------------------------------\\\
-        last_request = rospy.Time.now()
-
-        while not rospy.is_shutdown():
-
-            timeExceeded = rospy.get_rostime() - last_request > rospy.Duration(5)
-            if current_state.mode != "OFFBOARD" and timeExceeded:
-                set_mode_client.call(0, "OFFBOARD")
-                last_request = rospy.Time.now()
-
-            elif not current_state.armed and timeExceeded:
-                arming_client.call(True)
-                last_request = rospy.Time.now()
-
-
-            print("takeoff: "+str(takeoff))
-            print("fordward: "+str(fordward))
-            print("rotation: "+str(rotation))
-
-            # Pilot drone
-            if takeoff:
-                print("State: takeoff")
-                if current_pose.pose.position.z < desired_pose.pose.position.z - 0.2 or current_pose.pose.position.z > desired_pose.pose.position.z + 0.2:    
+                for i in range(1,25):
                     pose_pub.publish(desired_pose)
-                else:
-                    takeoff = False
+                    rate.sleep()
 
-            elif fordward:
-                print("State: fordward")
-                twist_pub.publish(desired_twist)
+                set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
+                response_setmode = set_mode_client.call(0, "OFFBOARD")
+                print("Response mode ------")
+                print(response_setmode)
+                print("------")
+            
+                arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+                response_arming = arming_client.call(True)
+                print("Response arm ------")
+                print(response_arming)
+                print("------")
 
-            elif rotation:
-                print("State: rotation")
-                pose_pub.publish(desired_pose)
+                print("-- OFFBOARD MODE TURNED ON --")
 
+                # Keeping OFFBOARD mode alive ---------------------------------------------------------------\\\
+                last_request = rospy.Time.now()
+
+                while not rospy.is_shutdown():
+
+                    time_exceeded = rospy.get_rostime() - last_request > rospy.Duration(5)
+                    if current_state.mode != "OFFBOARD" and time_exceeded:
+                        set_mode_client.call(0, "OFFBOARD")
+                        last_request = rospy.Time.now()
+
+                    elif not current_state.armed and time_Exceeded:
+                        arming_client.call(True)
+                        last_request = rospy.Time.now()
+
+
+                    # Send pose
+                    pose_pub.publish(desired_pose)
+                
+                    rate.sleep()
+                    print(offboard_mode)
+                    if offboard_mode == True:
+                        print "El offb es true"
+                    else:
+                        print "El offb se ha puesto a false"
+                        break
+
+                # Keeping OFFBOARD mode alive ---------------------------------------------------------------///
+                print("Hemos salido del bucle")
             else:
-                print("State: none")
-                twist_pub.publish(defaultTwist)
-        
-            rate.sleep()
+                print("Estamos en el ELSE")
+                time.sleep(1)
 
-        # Keeping OFFBOARD mode alive ---------------------------------------------------------------///
-
-    except rospy.ROSException:
+    except Exception as e:
         print("Exception on Offboard_node")
+        print(e.message)
+
         pass
